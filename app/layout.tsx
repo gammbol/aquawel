031fc4c3214d956600817578fa1a6b723511364c
tryingtothink <gammbol2525@gmail.com>
2025-11-18 16:18:19 +0300

    mb this time..?

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,11 @@ export default function RootLayout({
     <html lang="en">
     <head>
       {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-      <script src="https://kit.fontawesome.com/9a4581b7b4.js" crossOrigin="anonymous"></script>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        crossOrigin="anonymous"
+      />
     </head>
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
